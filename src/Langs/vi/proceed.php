67763768c7d0f2ab48ad4e54effe3f50b065c50c
unknown <game.oh.vn@gmail.com>
2019-10-04 15:27:06 +0700

    f

--- src/Langs/vi/proceed.php
+++ src/Langs/vi/proceed.php
@@ -8,5 +8,15 @@ return [
         0 => 'Thiết bị này đã vừa thực hiện xúc tiến, cần đợi vài phút trước khi tiếp tục',
         1 => 'Lỗi xảy ra, mời liên hệ hỗ trợ.'
     ],
-    'exception' => 'Lỗi xảy ra. Thử lại trước khi liên hệ hỗ trợ.'
+    'exception' => 'Lỗi xảy ra. Thử lại trước khi liên hệ hỗ trợ.',
+    'exchange' => [
+        'success' => 'Đổi điểm thành công, hãy kiểm tra lịch sử nạp/chuyển',
+        'fail' => [
+            -1 => 'Bạn có khả năng click chuột với tốc độ ánh sáng?',
+            0 => 'Đổi điểm thành công, hãy kiểm tra lịch sử nạp/chuyển',
+            1 => 'Chưa có điểm để chuyển',
+            2 => 'Đổi điểm không thành công, thử lại trước khi liên hệ hỗ trợ',
+        ],
+        'exception' => 'Lỗi xảy ra, liên hệ hỗ trợ.'
+    ],
 ];
